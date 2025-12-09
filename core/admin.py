2442aa6a5a88f7489d2b70422847b3899468e47f
Joy Yeon-ae <135769842+hyoonie@users.noreply.github.com>
2025-12-08 18:52:32 -0600

    adjusment

--- core/admin.py
+++ core/admin.py
@@ -2,11 +2,10 @@
 
 from django.contrib import admin
 # Importa todos los modelos que quieres ver
-from .models import Usuario, Dispositivo, Lecturas, TipoUsuario, Enfermedad
+from .models import Usuario, Dispositivo, TipoUsuario, Enfermedad
 
 # Registra cada modelo en el sitio de admin
 admin.site.register(Usuario)
 admin.site.register(Dispositivo)
-admin.site.register(Lecturas)
 admin.site.register(TipoUsuario)
 admin.site.register(Enfermedad)

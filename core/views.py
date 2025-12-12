bbcc8af119bc72bf185e84af7983a8eb6f0fbc9b
Joy Yeon-ae <135769842+hyoonie@users.noreply.github.com>
2025-12-12 00:11:10 -0600

    hotfix

--- core/views.py
+++ core/views.py
@@ -137,7 +137,7 @@ def view_ct_datos(request):
             if start_date_str and end_date_str:
                 try:
                     start_dt = datetime.strptime(start_date_str, '%Y-%m-%d')
-                    end_dt = datetime.strptime(end_date_str, '%Y-%m-%d') + datetime.timedelta(days=1) - datetime.timedelta(seconds=1)
+                    end_dt = datetime.strptime(end_date_str, '%Y-%m-%d') + timedelta(days=1) - timedelta(seconds=1)
                     mongo_query['fecha'] = {'$gte': start_dt, '$lte': end_dt}
                 except ValueError: pass
 

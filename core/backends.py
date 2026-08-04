a07d9c06d9fdd898e6ae27963a02ea7654023ad8
pola1497 <topola_14@hotmail.com>
2026-08-04 15:10:56 -0600

    feat: permitir login con email o usuario

---
+++ core/backends.py
@@ -0,0 +1,31 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.backends import ModelBackend
+from django.db.models import Q
+
+
+class UsuarioOrEmailBackend(ModelBackend):
+    def authenticate(self, request, username=None, password=None, **kwargs):
+        if username is None:
+            username = kwargs.get(get_user_model().USERNAME_FIELD)
+        if username is None or password is None:
+            return None
+
+        UserModel = get_user_model()
+        try:
+            user = UserModel.objects.get(
+                Q(usuario__iexact=username) | Q(email__iexact=username)
+            )
+        except UserModel.DoesNotExist:
+            return None
+        except UserModel.MultipleObjectsReturned:
+            user = (
+                UserModel.objects.filter(
+                    Q(usuario__iexact=username) | Q(email__iexact=username)
+                )
+                .order_by("id")
+                .first()
+            )
+
+        if user.check_password(password) and self.user_can_authenticate(user):
+            return user
+        return None
